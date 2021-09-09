84ba41d06760bfcc1f81867016fe037f00781389
Johnston, Peter <peter.johnston@wizards.com>
2021-09-09 14:58:01 -0700

    Update util.js

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,23 @@
 function Util() {
+	function calculateTotalDistributedPassengers(distributedPassengers) {
+		// return distributedPassengers.reduce(function(acc, val) { return acc + val; }, 0)
+		let totalDistributedPassengers = 0;
+		let values;
+		for(value in distributedPassengers) {
+			totalDistributedPassengers += distributedPassengers[value];
+		}
+		return distributedPassengers;
+	}
 
+	function calculateTotalNumberOfPassengers(ppassengersArray) {
+		let totalNumberOfpassengers = 0;
+		let passengers;
+		for (passengers in ppassengersArray) {
+			totalNumberOfPassengers += passengers;
+		}
+		return totalNumberOfPassengers;
+	}
+	return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 }
+module.exports = Util();
 
