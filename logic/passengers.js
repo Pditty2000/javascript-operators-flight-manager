04bc7e683385fba2512f04b529c1a4b4f37d89d2
Johnston, Peter <peter.johnston@wizards.com>
2021-09-13 11:01:58 -0700

    Update passengers.js

--- logic/passengers.js
+++ logic/passengers.js
@@ -22,26 +22,27 @@ function Passengers() {
 	// return {checkFlightCapacity};
 
 	function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight) {
-		let vipInBusiness = 0, vipInEconomy = 0, regularInBusiness = 0, regularInEconomy = 0;
+		let vipPassengersWithBusinessSeats = 0, vipPassengersWithEconomySeats = 0, 
+		regularPassengersWithBusinessSeats = 0, regularPassengersWithEconomySeats = 0;
 		let businessSeatsRemaining = nrOfFlights * businessSeatsPerFlight;
 		let economySeatsRemaining = nrOfFlights * economySeatsPerFlight;
 
 		var vipBusinessConfiguration = {passengers:vipPassengers, seats:businessSeatsRemaining};
-		vipInBusiness = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
+		vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
 
 		var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers, seats:economySeatsRemaining};
 		vipInEconomy = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
 
 		var regularBusinessConfiguration = {passengers:regularPassengers, seats: vipBusinessConfiguration.seats};
-		regularInBusiness = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
+		regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
 
 		var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers, seats: vipEconomyConfiguration.seats};
-		regularInEconomy = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
+		regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
 
-		return {vipInBusiness:vipInBusiness, vipInEconomy: vipInEconomy, regularInBusiness: regularInBusiness, regularInEconomy:regularInEconomy};
+		return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats, vipPassengersWithEconomySeats: vipPassengersWithEconomySeats, regularInBusiness: regularInBusiness, regularInEconomy:regularInEconomy};
 	}
 
-	function updateConfiguration(configuration, seatsPerFlight) {
+	function updateConfiguration(configuration, seatsPerFlight) {	
 		let passengersWithSeats = 0;
 		while (configuration.passengers > 0) {
 			if (configuration.seats > 0) {
